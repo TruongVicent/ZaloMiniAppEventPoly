9c84e62678b941916ecaea5c8216c1d363d0a90b
Minh-Truong <nguyenmingtruong9@gmail.com>
2024-11-13 12:07:00 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TickitController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
